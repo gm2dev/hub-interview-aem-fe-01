c058228bf496e40a9b49851377b9e35fd40acac6
kevinbrinach <kevinbrinach@gmail.com>
2025-10-02 15:36:18 -0300

    chore: remove uneeded stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,7 @@
-import { useState } from "react";
-import { List } from "./components/List";
-import { generateItems, moveItemInList } from "./utils";
-import { Item } from "./types";
-
-const items = generateItems(4);
-const initialOrder = items.map((item) => item.id);
-
-interface TList {
-  id: string;
-  order: Item['id'][];
-}
+import React from "react";
 
 const App = () => {
-  const [list, setList] = useState<TList>({
-    id: "list-1",
-    order: initialOrder,
-  });
-
-  const handleMoveItem = (fromIndex: number, targetIndex: number) =>
-    setList((prev) => ({
-      id: prev.id,
-      order: moveItemInList({
-        list: prev.order,
-        fromIndex,
-        targetIndex,
-      }),
-    }));
-
-  const getSortedItemsForList = () =>
-    list.order.map((itemId) =>
-      items.find((item) => item.id === itemId)
-    ) as Item[];
-
-  return <List items={getSortedItemsForList()} onMoveItem={handleMoveItem} />;
+  return <div>Hello</div>;
 };
 
 export default App;
